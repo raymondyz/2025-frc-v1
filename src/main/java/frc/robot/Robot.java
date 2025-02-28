1dd479e935e03af20f6c8bfd16c5be54a6d3f7a3
Raymond Zou <raymond.yh.zou@gmail.com>
2025-02-28 09:43:37 -0800

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -198,15 +198,15 @@ public class Robot extends TimedRobot {
 
     // Shoot Coral
     
-    // if (timer.get() > initialPauseTime + driveForwardTime + 4) {
-    //   robotDrive.arcadeDrive(0, 0)
-    // }
-    // if (timer.get() > initialPauseTime + driveForwardTime + 1.5) {
-    //   robotDrive.arcadeDrive(-0.5, rotation);
-    // }
-    // else if (timer.get() > initialPauseTime + driveForwardTime + 1) {
-    //   shooter.set(0.65);
-    // }
+    if (timer.get() > initialPauseTime + driveForwardTime + 5) {
+      robotDrive.arcadeDrive(0, 0);
+    }
+    if (timer.get() > initialPauseTime + driveForwardTime + 2.5) {
+      robotDrive.arcadeDrive(-0.5, rotation);
+    }
+    else if (timer.get() > initialPauseTime + driveForwardTime + 2) {
+      shooter.set(0.65);
+    }
     
   }
 
